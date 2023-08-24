df8011ba4aefe508f0d283525b2c0463d4951c42
Lindolfo Moizinho <lindolfomoizinho1@gmail.com>
2023-08-24 08:04:57 -0300

    Initial commit and first configurations

---
+++ src/test/java/br/com/libdolf/gamereview/GameReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.libdolf.gamereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
